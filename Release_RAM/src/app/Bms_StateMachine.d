fd4bc17a3dbf379d613fe5fe52fee05100da3509
Jingyu <jv@jv.com>
2026-08-17 12:57:28 -0700

    StateMachine + LED

--- Release_RAM/src/app/Bms_StateMachine.d
+++ Release_RAM/src/app/Bms_StateMachine.d
@@ -4,10 +4,20 @@ src/app/Bms_StateMachine.o: ../src/app/Bms_StateMachine.c \
  C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Platform_Types.h \
  C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Compiler.h \
  C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Compiler_Cfg.h \
- C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/CompilerDefinition.h
+ C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/CompilerDefinition.h \
+ ../RTD/include/Siul2_Dio_Ip.h \
+ C\:/S32K344/workspace/BMS_demo/generate/include/Siul2_Dio_Ip_Cfg.h \
+ C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K344_SIUL2.h \
+ C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K344_COMMON.h \
+ C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Dio_MemMap.h
 ../src/app/Bms_StateMachine.h:
 C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Std_Types.h:
 C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Platform_Types.h:
 C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Compiler.h:
 C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Compiler_Cfg.h:
 C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/CompilerDefinition.h:
+../RTD/include/Siul2_Dio_Ip.h:
+C\:/S32K344/workspace/BMS_demo/generate/include/Siul2_Dio_Ip_Cfg.h:
+C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K344_SIUL2.h:
+C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/header/S32K344_COMMON.h:
+C\:/NXP/S32DS.3.6.10/S32DS/software/PlatformSDK_S32K3/RTD/BaseNXP_TS_T40D34M70I1R0/include/Dio_MemMap.h:
--- src/app/Bms_StateMachine.c
+++ src/app/Bms_StateMachine.c
@@ -1,4 +1,9 @@
 #include "Bms_StateMachine.h"
+#include "Siul2_Dio_Ip.h"
+#include "Siul2_Dio_Ip_Cfg.h"
+
+#define BMS_STATE_LED_PORT    PTA_H_HALF
+#define BMS_STATE_LED_PIN     (15U)
 
 /*
  * Temporary external CAN requests.
@@ -18,6 +23,37 @@ void Bms_StateMachine_Init(void)
 Bms_StateType Bms_StateMachine_GetState(void)
 {
     return g_BmsState;
+}
+
+static void Bms_StateMachine_UpdateLed(void)
+{
+    switch (g_BmsState)
+    {
+        case BMS_STATE_ACTIVE:
+        {
+            /* LED3 ON - active low */
+            Siul2_Dio_Ip_WritePin(
+                BMS_STATE_LED_PORT,
+                BMS_STATE_LED_PIN,
+                0U
+            );
+            break;
+        }
+
+        case BMS_STATE_INIT:
+        case BMS_STATE_STANDBY:
+        case BMS_STATE_FAULT:
+        default:
+        {
+            /* LED3 OFF */
+            Siul2_Dio_Ip_WritePin(
+                BMS_STATE_LED_PORT,
+                BMS_STATE_LED_PIN,
+                1U
+            );
+            break;
+        }
+    }
 }
 
 void Bms_StateMachine_MainFunction(void)
@@ -79,4 +115,6 @@ void Bms_StateMachine_MainFunction(void)
             break;
         }
     }
+
+    Bms_StateMachine_UpdateLed();
 }
